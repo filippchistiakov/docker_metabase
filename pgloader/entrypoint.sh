55b4d288574a73f2d7bf5c339610f819d0c5c93e
Filipp <philipp.chistykov@gmail.com>
2024-06-10 22:33:15 +0200

    Раскомитил код проверки инциализации postgres

--- pgloader/entrypoint.sh
+++ pgloader/entrypoint.sh
@@ -11,9 +11,6 @@ wait_for_service() {
     done
     echo "$service is ready!"
 }
-# docker run -it --entrypoint=/bin/bash postgres:13
-# find / -name docker-entrypoint.sh
-
 # Подождать, пока PostgreSQL полностью инициализируется
-#wait_for_service "PostgreSQL" "localhost" "5432"
+wait_for_service "PostgreSQL" "postgres_13_meta" "5432"
 pgloader --debug sqlite://dir_db/data.db postgresql://metabase:metabase@postgres_13_meta:5432/video
